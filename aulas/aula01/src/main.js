b58d9b6dcb422d5cbbebb34a9565434540524847
Jose Reginaldo <prof.josereginaldo@gmail.com>
2026-03-19 21:48:57 -0300

    implemente validacao do login

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,37 @@
-import './style.css'
+import "./style.css";
+
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", (e) => {
+  const matriculaInput = document.querySelector("#matricula");
+  const matriculaErro = document.querySelector("#matriculaErro");
+  const senhaInput = document.querySelector("#senha");
+  const senhaErro = document.querySelector("#senhaErro");
+ 
+  matriculaErro.textContent = '';
+  senhaErro.textContent = '';
+
+  if (matriculaInput.value == '') {
+    matriculaErro.textContent = 
+        "Matrícula é obrigatória";
+    return;
+  }
+
+  if (parseInt(matriculaInput.value) <= 0) {
+    matriculaErro.textContent = 
+        "Matricula deve ser um numero positivo";
+    return;
+  }
+
+  if (senhaInput.value == '') {
+    senhaErro.textContent = "Senha é obrigatoria";
+    return;
+  }
+
+  if (senhaInput.value.length < 8) {
+    senhaErro.textContent = 
+        "A Senha deve conter pelo menos 8 caracters";
+    return;
+  }
+
+});
