5f6edd1934326c3795d430ab544f46e5eb102d9f
Jose Reginaldo <prof.josereginaldo@gmail.com>
2026-05-21 21:44:00 -0300

    adiciona listagem com useeffect

--- aulas/aula04/src/pages/Home.jsx
+++ aulas/aula04/src/pages/Home.jsx
@@ -1,7 +1,82 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+  const [carregando, setCarregando] = useState(true);
+  const [tarefas, setTarefas] = useState([]);
+
+  const carregarDados = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setTarefas([
+      { id: 1, nome: "Fazer o trabalho", status: "aberta" },
+      { id: 2, nome: "Estudar API REST", status: "aberta" },
+      { id: 3, nome: "Entregar as práticas", status: "pendente" },
+      { id: 4, nome: "Revisar conteudo", status: "pendente" },
+      { id: 5, nome: "Assistir video-aulas", status: "concluida" },
+    ]);
+  };
+
+  useEffect(() => {
+    const disparar = async () => {
+      await carregarDados();
+      setCarregando(false);
+    };
+    disparar();
+  }, []);
+
   return (
     <>
       <h1>Página Inicial</h1>
+      <h2>Minhas Tarefas do Dia</h2>
+      {carregando ? (
+        <>
+          <p>Aguarde...</p>
+          <table className="border border-gray-300 rounded animate-pulse">
+            <thead>
+              <tr className="bg-gray-300">
+                <th className="py-2 px-8"></th>
+                <th className="py-2 px-8"></th>
+                <th className="py-2 px-8"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+              </tr>
+              <tr>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+              </tr>
+              <tr>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+                <td><div className="my-1 mx-1 py-0.5 rounded bg-gray-300"></div></td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Tarefa</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tarefas.map((item, index) => (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
--- aulas/aula04/src/pages/Perfil.jsx
+++ aulas/aula04/src/pages/Perfil.jsx
@@ -3,7 +3,11 @@ import { useForm } from "react-hook-form";
 
 function Perfil() {
   const { id } = useParams();
-  const { register, handleSubmit, formState: {errors} } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   // register() => {name="", onChange="", onBlur=""}
   // handleSubmit() => dispara a validacao,
   // informa ao React que mudou os campos
@@ -13,10 +17,33 @@ function Perfil() {
   // buscar os dados do usuário
 
   const regras = {
-    nome: { required: "Nome é obrigatório" },
-    email: { required: "Email é obrigatório" },
-    nascimento: {},
-    telefone: {},
+    nome: {
+      required: "Nome é obrigatório",
+      minLength: { value: 3, message: "Nome tem no mínimo 3 caracteres" },
+      maxLength: { value: 100, message: "Nome tem no máximo 100 caracteres" },
+    },
+    email: {
+      required: "Email é obrigatório",
+      pattern: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message: "Email inválido ",
+      },
+    },
+    nascimento: {
+      validate: {
+        dataMinima: (value) =>
+          Date.parse(`${value} 00:00:00 UTC`) >= new Date("01/01/1900 00:00:00 UTC").getTime() || "Data inferior",
+        dataMaxima: (value) =>
+          Date.parse(`${value} 00:00:00 UTC`) < new Date().getTime() || "Data superior",
+      },
+    },
+    telefone: {
+      min: { value: 0, message: "Telefone invalido" },
+      pattern: {
+        value: /^(\(?\d{2}\)?[\s.-]?)?(\d{4,5})[\s.-]?(\d{4})$/,
+        message: "Telefone invalido",
+      },
+    },
   };
 
   return (
@@ -25,21 +52,13 @@ function Perfil() {
       <form onSubmit={handleSubmit(salvar)}>
         <div>
           <label htmlFor="nome">Nome</label>
-          <input 
-            type="text" 
-            id="nome" 
-            {...register("nome", regras.nome)} 
-          />
-          { errors?.nome && <p>{ errors.nome.message }</p> }
+          <input type="text" id="nome" {...register("nome", regras.nome)} />
+          {errors?.nome && <p>{errors.nome.message}</p>}
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input 
-            type="email" 
-            id="email" 
-            {...register("email", regras.email)} 
-          />
-          { errors?.email && <p>{ errors.email.message }</p> }
+          <input type="email" id="email" {...register("email", regras.email)} />
+          {errors?.email && <p>{errors.email.message}</p>}
         </div>
         <div>
           <label htmlFor="nascimento">Data de Nascimento</label>
@@ -48,6 +67,7 @@ function Perfil() {
             id="nascimento"
             {...register("nascimento", regras.nascimento)}
           />
+          {errors?.nascimento && <p>{errors.nascimento.message}</p>}
         </div>
         <div>
           <label htmlFor="telefone">Telefone</label>
@@ -56,6 +76,7 @@ function Perfil() {
             id="telefone"
             {...register("telefone", regras.telefone)}
           />
+          {errors?.telefone && <p>{errors.telefone.message}</p>}
         </div>
         <button type="submit">Salvar</button>
       </form>
